7b87aa526b9232914ffd84fc9c7944cb9bea191c
ValKalMey <kalpers.meyers@gmail.com>
2026-04-21 10:35:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'roadtrip-thai-v1';
+const ASSETS = [
+  '/index.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Bubblegum+Sans&display=swap'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
